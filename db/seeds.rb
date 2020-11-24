622aab08d623859731b16e5a069816f56e35e43d
mhcrocky <larastar721@gmail.com>
2020-11-24 02:15:56 -0500

    Update seeds
    pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "user@example.com", password: "password", password_confirmation: "password")
+15.times do |i|
+  User.create(email: "user#{i+1}@example.com", password: "bs&E2wJC7A(?4X!", password_confirmation: "bs&E2wJC7A(?4X!")
+end
 
 User.all.each do |u|
-  12.times do |i|
+  15.times do |i|
     u.todo_items.create(title: "To Do Item #{i+1} for #{u.email}", complete: i % 3  == 0 ? true : false  )
   end
 end
 
 User.all.each do |u|
-  12.times do |i|
+  1.times do |i|
     u.articles.create(title: "Article #{i+1}", text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.")
   end
 end
