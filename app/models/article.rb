c5a8648c23ea57f8f531db9ee3dcb7eb996ad1bd
mhcrocky <larastar721@gmail.com>
2020-12-01 03:47:48 -0500

    Added author search with left outer join

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,18 +1,26 @@
 class Article < ApplicationRecord
 
-  has_rich_text :text
-
   # default_scope { order(created_at: :desc) }
 
   belongs_to :user
+
+  validates_presence_of :user
 
   validates :title, presence: true
 
   mount_uploader :avatar, AvatarUploader
 
+  has_rich_text :text
+
   def self.search(search)
     if search && search.length > 0
-      where("lower(title) LIKE ? OR lower(text) LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%")
+      select('articles.*, lower(articles.title)').
+      joins("LEFT OUTER JOIN users ON users.id  = articles.user_id").
+        where("lower(articles.title) LIKE ? OR lower(articles.text) LIKE ? OR lower(users.email) LIKE ?",
+          "%#{search.downcase}%",
+          "%#{search.downcase}%",
+          "%#{search.downcase}%").
+        distinct
     else
       where(nil)
     end
