dda1a5a09d3d4fc983eb081e8b7fc7fd96d67a96
mhcrocky <larastar721@gmail.com>
2020-12-03 02:05:16 -0500

    Individual todo items progress chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -12,9 +12,11 @@ class ChartsController < ApplicationController
 
   ## Todo Items
   def complete_incomplete_todos
-    render json: TodoItem.group(:complete).count    
+    render json: TodoItem.group(:complete).count
   end
-
+  def complete_incomplete_single_todos
+    render json: current_user.todo_items.group(:complete).count
+  end
   def month_of_year_todos
     render json: TodoItem.group_by_month_of_year(:created_at).count.map{ |k, v| [I18n.t("date.month_names")[k], v] }
   end
