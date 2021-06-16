557e6fb9182d2bc97fa8148986685d5373153a65
mhcrocky <larastar721@gmail.com>
2021-06-16 12:21:26 -0400

    Set User in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
     @articles = Article.where(user_id: @user.id).
                         order('created_at DESC').
                         paginate(page: params[:page], per_page: 12)
   end
 
   def toggle_theme
-    @user = User.find(params[:id])
     @user.toggle!(:dark_theme)
     render json: { result: true }
   end
 
   def destroy
-    @user = User.find(params[:id])
-    if can? :manage, @user
+    if can? :manage, @user # Prevent inserted delete button as html from working
       @user.destroy
       respond_to do |format|
         format.html { redirect_to request.referrer, notice: 'User was successfully deleted.' }
@@ -24,6 +22,12 @@ class UsersController < ApplicationController
     else
       redirect_to authenticated_root_url, alert: 'You CANNOT delete a user other than yourself. If you want to delete your account, please click on Edit Profile in the top menu.'
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
