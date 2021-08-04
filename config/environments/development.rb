03aed144c58ae72a94ab58ecc8782412b43fb6c9
mhcrocky <larastar721@gmail.com>
2021-08-03 21:29:31 -0400

    Confirmation email styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # # Show Devise emails
+  # config.action_mailer.preview_path = "#{Rails.root}/app/mailer_previews"
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
