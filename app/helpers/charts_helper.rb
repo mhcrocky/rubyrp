b8f692a3deed974e76327fc426a125ad2e205d73
mhcrocky <larastar721@gmail.com>
2021-08-25 00:28:28 -0400

    Cleaned up layout

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,5 +1,41 @@
 module ChartsHelper
 
+  # Returns a default array of colors for charts
+  def chart_colors
+    return [
+      "rgba(0,255,255,0.9)",
+      "rgba(111,71,190,0.7)",
+      "rgba(255,0,0,0.7)",
+      "rgba(0,255,0,0.7)",
+      "rgba(0,0,255,0.7)",
+      "rgba(61,12,2,0.7)",
+      "rgba(251,79,20,0.9)",
+      "rgba(230,67,140,0.9)",
+      "rgba(247, 202, 24, 0.9)",
+      "rgba(40,107,230,0.6)",
+      "rgba(254,241,96,0.6)",
+      "rgba(51,110,123,0.6)",
+      "rgba(33,51,44,0.6)",
+      "rgba(0,255,255,0.6)",
+      "rgba(24,69,55,0.6)",
+      "rgba(111,71,190,0.9)",
+      "rgba(255,0,0,0.9)",
+      "rgba(251,79,20,0.9)",
+      "rgba(0,255,0,0.9)",
+      "rgba(40,107,230,0.9)",
+      "rgba(254,241,96,0.9)",
+      "rgba(51,110,123,0.9)",
+      "(220,27,222,0.9)",
+      "rgba(33,51,44,0.9)",
+      "rgba(126,178,189,0.9)",
+      "rgba(0,255,255,0.9)",
+      "rgba(177,42,91,0.9)",
+      "rgba(7,43,61,0.9)",
+      "rgba(24,69,55,0.9)",
+      "rgba(61,22,53,0.9)"
+    ]
+  end
+
   # Returns a default array of colors for Chat charts
   def chat_chart_colors
     return [
@@ -8,16 +44,6 @@ module ChartsHelper
       "rgba(230,67,140,0.5)",
       "rgba(230,67,140,0.3)",
       "rgba(230,67,140,0.1)",
-      "rgba(0,0,0,0.3)",
-      "rgba(0,0,0,0.5)",
-      "rgba(0,0,0,0.7)",
-      "rgba(0,0,0,0.9)"
-    ]
-  end
-
-  # Returns a default array of colors for charts
-  def chart_colors
-    return [
       "rgba(0,255,255,0.9)",
       "rgba(111,71,190,0.7)",
       "rgba(255,0,0,0.7)",
