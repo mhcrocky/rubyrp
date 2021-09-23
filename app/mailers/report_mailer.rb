8457128ff60a4aa13aeac19ca3f0d8df30c13068
mhcrocky <larastar721@gmail.com>
2021-09-23 02:14:13 -0400

    Tightened up analytics email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -4,19 +4,17 @@ class ReportMailer < ApplicationMailer
   #
   #   en.report_mailer.daily.subject
 
-  # Daily email for 2 example accounts + 'others' analytics.
+  # Daily analytics email
   # Run daily with Heroku Scheduler:  $ ReportMailer.daily.deliver_now
   def daily
-    # @email = ENV["GMAIL_USERNAME"]
+    # @user = ENV["GMAIL_USERNAME"]
     @user = User.with_role(:sysadmin)
                 .pluck(:email)
-                .first
 
     @visits = Ahoy::Visit.daily
                          .order(:started_at, id: :asc)
 
     mail to: @user
-    # mail to: @email #"to@example.org"
   end
 
 end
