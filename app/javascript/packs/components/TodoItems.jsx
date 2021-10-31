1ad71bdec3537e7326495f1f2b9195aa4aa93737
mhcrocky <larastar721@gmail.com>
2021-10-30 23:28:40 -0400

    Cleaned up Layout
    Bootstrap 5 stable enough for production testing

--- app/javascript/packs/components/TodoItems.jsx
+++ app/javascript/packs/components/TodoItems.jsx
@@ -13,7 +13,7 @@ class TodoItems extends React.Component {
     return (
       <>
         <button
-          className="w-100 btn btn-lg btn-outline-secondary mb-1"
+          className="w-100 btn btn-lg btn-secondary mb-1"
           onClick={this.handleClick}
         >
         {this.props.hideCompletedTodoItems
