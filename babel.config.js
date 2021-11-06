a600e307daaebe7780f2e278ef0a7cb2e1e00570
mhcrocky <larastar721@gmail.com>
2021-11-06 05:33:18 -0400

    Update babel.config.js
    loose

--- babel.config.js
+++ babel.config.js
@@ -51,6 +51,9 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
+        "@babel/plugin-proposal-private-property-in-object", { "loose": true }
+      ],
+      [
         '@babel/plugin-proposal-class-properties',
         {
           loose: true
