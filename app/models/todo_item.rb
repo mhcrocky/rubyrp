729c5c0e4f9294d5d9f95c260a78885afc3c4674
mhcrocky <larastar721@gmail.com>
2022-01-02 16:56:18 -0500

    Blank-padded month numbers

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -7,12 +7,12 @@ class TodoItem < ApplicationRecord
 
   # Returns a string .. formatted TodoItem created_at field
   def created
-    "#{self.created_at.in_time_zone("#{self.user.timezone}").strftime('%m/%d/%Y')}"
+    "#{self.created_at.in_time_zone("#{self.user.timezone}").strftime('%-m/%d/%Y')}"
   end
 
   # Returns a string .. formatted TodoItem updated_at field
   def updated
-    "#{self.updated_at.in_time_zone("#{self.user.timezone}").strftime('%m/%d/%Y')}" # .. %H, %M, %S
+    "#{self.updated_at.in_time_zone("#{self.user.timezone}").strftime('%-m/%d/%Y')}" # .. %H, %M, %S
   end
 
   # Returns a string .. strip out unknown characters, etc.. for title in word tree
